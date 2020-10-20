2ba84f0b242bfd977527fda00bbb280326495793
charlietran01 <charlietran01@gmail.com>
2020-10-20 04:27:11 -0500

    Knife can cut

--- Assets/Scripts/KnifeUse.cs
+++ Assets/Scripts/KnifeUse.cs
@@ -9,11 +9,14 @@ public class KnifeUse : MonoBehaviour
     public GameObject rightBread;
     public void OnTriggerEnter(Collider bread)
     {
-        Vector3 leftBreadPosition = new Vector3(-0.839f, 1.199f, -0.65f);
-        Vector3 rightBreadPosition = new Vector3(-0.839f, 1.199f, -0.43f);
-        Destroy(bread);
-        Instantiate(leftBread, leftBreadPosition, Quaternion.AngleAxis(90, Vector3.right));
-        Instantiate(rightBread, rightBreadPosition, Quaternion.AngleAxis(90, Vector3.right));
+        if(bread.gameObject.tag == "Bread")
+        {
+            Destroy(bread.gameObject);
+            Vector3 leftBreadPosition = new Vector3(-0.839f, 1.199f, -0.65f);
+            Vector3 rightBreadPosition = new Vector3(-0.839f, 1.199f, -0.43f);
+            Instantiate(leftBread, leftBreadPosition, Quaternion.AngleAxis(90, Vector3.right));
+            Instantiate(rightBread, rightBreadPosition, Quaternion.AngleAxis(90, Vector3.right));
+        }
     }
     // Start is called before the first frame update
     void Start()
