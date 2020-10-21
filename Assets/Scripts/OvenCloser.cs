e2dd867f082dc86a7d2abab2247543d3f6e7cbd1
charlietran01 <charlietran01@gmail.com>
2020-10-21 12:41:34 -0500

    Revert "Revert "Merge branch 'main' of https://github.com/EmyBaby/Comp194Project into main""
    This reverts commit a47afa91e686992f6ed5999aac4ad8a6667e398e.

---
+++ Assets/Scripts/OvenCloser.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OvenCloser : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject door;
+    public GameObject openDoor;
+    public void OnTriggerEnter(Collider openDoor)
+    {
+        if(openDoor.gameObject.tag == "Open Door")
+        {
+            Destroy(openDoor.gameObject);
+            door.gameObject.SetActive(true);
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Stage1.cs
+++ Assets/Scripts/Stage1.cs
@@ -10,6 +10,8 @@ public class Stage1 : MonoBehaviour
     public GameObject cuttingBoard;
     public LineRenderer line;
     TMPro.TextMeshProUGUI guide;
+    GameObject soap;
+    GameObject sponge;
     List<string> UIText;
     bool triggerPressed;
     bool raycastMode;
@@ -23,6 +25,8 @@ public class Stage1 : MonoBehaviour
         panel.transform.GetChild(2).gameObject.SetActive(false);
         raycastMode = true;
         coolDown = 2;
+        soap = GameObject.Find("Soap");
+        sponge = GameObject.Find("Sponge");
         UIText = new List<string>();
         UIText.Add(@"Hello! Welcome to Kitchen Safety Training. This training will teach you a couple things about kitchen safety.
 The first and most basic concept that must be applied at all times is hygiene!");
@@ -38,7 +42,6 @@ Rinse your hands off for about 10 seconds until the soap washes off and dirt fal
 For your next task, you will repeat the same cleaning process with a cutting board and soapy sponge");
         UIText.Add(@"Wet the board for 5 seconds, rub it with the sponge for 5 seconds, then rinse it for 10 seconds.");
         UIText.Add(@"Good job! You now have basic experience with kitchen hygiene.");
-        textIndex = 5;
     }
 
     // Update is called once per frame
@@ -79,6 +82,11 @@ For your next task, you will repeat the same cleaning process with a cutting boa
             panel.transform.GetChild(1).gameObject.SetActive(false);
         }
 
+        if (textIndex == 1)
+        {
+            cuttingBoard.SetActive(false);
+        }
+
         if (textIndex >= 2 && textIndex <= 4)
         {
             raycastMode = false;
@@ -89,6 +97,9 @@ For your next task, you will repeat the same cleaning process with a cutting boa
         if (textIndex == 5)
         {
             GameObject.Find("Sink_Counter").transform.GetChild(4).GetChild(0).gameObject.GetComponent<Sink>().setTriggerMode(true);
+            soap.SetActive(false);
+            sponge.SetActive(true);
+            cuttingBoard.SetActive(true);
         }
 
         if (textIndex == 6)
@@ -96,7 +107,6 @@ For your next task, you will repeat the same cleaning process with a cutting boa
             raycastMode = false;
             WashObjectGUI();
         }
-        // textIndex = 6;
     }
 
     void FixedUpdate()
