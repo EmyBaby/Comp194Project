fbbe16b1dff3311a6dc3bfafa9079ca7f39183de
charlietran01 <charlietran01@gmail.com>
2020-10-22 01:05:41 -0500

    grabbing and putting chicken in oven

--- Assets/Scripts/ChickenGrabber.cs
+++ Assets/Scripts/ChickenGrabber.cs
@@ -4,8 +4,28 @@ using UnityEngine;
 
 public class ChickenGrabber : MonoBehaviour
 {
-    void OnCollisionEnter(Collision c) {
+    public GameObject chicken;
+    public GameObject tray;
+    public void OnTriggerEnter(Collider chicken)
+    {
+        if(chicken.gameObject.tag == "ChickenTray")
+        { 
+            //Destroy(chicken.gameObject);
+            Vector3 trayHolderPosition = gameObject.transform.position;
+            Quaternion trayHolderRotation = gameObject.transform.rotation;
+            //Instantiate(attachedChicken, trayHolderPosition, trayHolderRotation);
+            chicken.gameObject.transform.parent = tray.transform;
+        }
+    }
+    /*void Update()
+    {
+        Vector3 trayHolderPosition = gameObject.transform.position;
+        Quaternion trayHolderRotation = gameObject.transform.rotation;
+        attachedChicken.transform.position = trayHolderPosition;
+        attachedChicken.transform.rotation = trayHolderRotation;
+    }*/
+    /*void OnCollisionEnter(Collision c) {                  //Sticky collision
         var joint = gameObject.AddComponent<FixedJoint>();
         joint.connectedBody = c.rigidbody;
-    }
+    }*/
 }
